e960f28018bfacccc26f6d7129008147ebf5febf
Ruut <r.brandsma@ecofys.com>
2012-12-21 16:41:03 +0100

    made font smaller of tooltip and restore with of question list on non qmap pages

--- qa-qmap.js
+++ qa-qmap.js
@@ -2,12 +2,16 @@
 
 (function( $ ){
 	$.fn.copyHTMLtoTitleToolTip = function() {	
-	var $this = $(this)
-	$(".qa-q-item-main").width(250)
-	$this.each(function(index) {
+	var $this = $(this)	
+	$this.each(function(index) {		
 		var $myitem =$(this)
 		var $associate = $("#qmap-"+$myitem.attr('id'))
 		if ($associate.length>0) {
+			$(".qa-q-item-main").width(250)
+			$(".qa-q-item-title").css("font-size","12px")
+			$(".qa-q-item-meta").css("font-size","10px")			
+			$(".qa-downvote-count-data,.qa-upvote-count-data,.qa-a-count-data").css("font-size","12px")
+			$(".qa-downvote-count-pad,.qa-upvote-count-pad,.qa-a-count-pad").css("font-size","10px")	
 			$associate.attr("title",$myitem.html())
 			//$myitem.hide()
 			$myitem.remove()
